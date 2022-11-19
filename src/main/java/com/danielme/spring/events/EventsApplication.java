bbc605bb821956cb5d3ef32f50ca498b0144fc40
dani <danielme_com@yahoo.com>
2022-11-19 13:07:55 +0100

    ejemplo de ApplicationEventMulticaster (comentado)

--- src/main/java/com/danielme/spring/events/EventsApplication.java
+++ src/main/java/com/danielme/spring/events/EventsApplication.java
@@ -2,15 +2,27 @@ package com.danielme.spring.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.support.TaskUtils;
 
 @SpringBootApplication
 @EnableAsync
 public class EventsApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(EventsApplication.class, args);
 	}
+
+	//Use @Async instead of creating ApplicationEventMulticaster.
+	/*@Bean
+	ApplicationEventMulticaster applicationEventMulticaster() {
+		SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+		eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+		return eventMulticaster;
+	}*/
 
 }
